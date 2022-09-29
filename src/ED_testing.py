18019ec80f604e3368d14c10652d5a0f4954bed8
joaovaleriano <joaopedrovm98@gmail.com>
2022-09-29 10:38:48 +0100

    subsampler

--- src/ED_testing.py
+++ src/ED_testing.py
@@ -6,3 +6,29 @@
 
 Functions for generating multiple experimental design conditions
 """
+
+
+#%%
+# packages
+
+import numpy as np
+
+
+#%%
+
+def subsampler(t, tp, xp):
+    """
+    subsampling: samples a trajectory xp, orignially measured at time points tp, on points t, by linear interpolation
+    
+    --- INPUT ---
+    t: time points where to sample. array (P,)
+    tp: original timepoints. array (N,)
+    xp: measured trajectory over points tp. array (N, M)
+
+    --- OUTPUT ---
+    x:
+    """
+
+    x = np.array([np.interp(t, tp, xp[:,i]) for i in range(xp.shape[1])]).T
+
+    return x
