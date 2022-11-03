37c9c3a05a90a2cbcd62e2d14317ed94f46eb49f
joaovaleriano <joaopedrovm98@gmail.com>
2022-11-03 13:23:17 +0000

    .

--- src/generate_data.py
+++ src/generate_data.py
@@ -28,16 +28,17 @@ from analysis import *
 #%%
 # define dataset properties
 
-n_sp = np.array([2, 3, 4, 5])
+n_sp = np.array([2, 4, 6, 8, 10])
 n_samples = [5, 10, 20, 50, 100]
 t_samp_list = [np.linspace(0, 30, i) for i in n_samples]
 
-params_seeds = np.arange(100)
+params_seeds = np.arange(20)
 
 env_noise_list = [0., 0.05, 0.1, 0.2]
-meas_noise_list = [0., 0.05, 0.1, 0.2]
+meas_noise_list = [0.1, 0.05, 0.1, 0.2]
 
-n_init_cond = 5
+n_init_cond = 20
+perturb_scale_and_sigma = [-0.5, 1]
 repetitions = 1
 
 t0 = 0.
@@ -48,12 +49,19 @@ A_off_diag_std = 1.
 A_diag_mean = A_off_diag_std * n_sp**0.5 * 1.1
 A_diag_std = 0.1 * A_diag_mean
 
+
+#%%
+
+n_experiments = len(n_sp) * len(n_samples) * len(params_seeds) * len(env_noise_list) * len(meas_noise_list) * n_init_cond * repetitions
 data_size = n_sp.sum() * np.sum(n_samples) * len(params_seeds) * len(env_noise_list) * len(meas_noise_list) * n_init_cond * repetitions * 8 / 1024**2
+print(f"Number of experiments: {n_experiments}")
 print(f"Expected total dataset size: {data_size:.3f} MB")
 
 
 #%%
 # data generation command
+
+np.random.seed(0)
 
 for i in range(len(n_sp)):
     print(f"{n_sp[i]} species: ")
@@ -64,9 +72,10 @@ for i in range(len(n_sp)):
         A = p[n_sp[i]:].reshape((n_sp[i], n_sp[i]))
         x_eq = -np.linalg.inv(A)@r
 
-        init_cond_list = init_cond_by_perturb(x_eq, 2, [[-1, 1]], 1)
+        init_cond_list = init_cond_by_perturb(x_eq, 2, perturb_scale_and_sigma, n_init_cond)
 
-        for env_noise in env_noise_list:
+        for k in range(len(env_noise_list)):
+            env_noise = env_noise_list[k]
             save_name = f"{n_sp[i]}_sp{j}_env_noise{env_noise}"
-            gen_replicates(p, env_noise, init_cond_list, t0, dt, t_samp_list, meas_noise_list, repetitions, seed=0, scale_meas_noise_by_abund=True, save_datasets=True, save_loc="test", save_name=save_name)
+            gen_replicates(p, env_noise, init_cond_list, t0, dt, t_samp_list, meas_noise_list, repetitions, seed=k, scale_meas_noise_by_abund=True, save_datasets=True, save_loc="test", save_name=save_name)
 
