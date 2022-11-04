bf4e8d184d6165dd9d8bd7b9fc3be349561f40a4
joaovaleriano <joaopedrovm98@gmail.com>
2022-11-04 11:21:51 +0000

    .

--- src/generate_data.py
+++ src/generate_data.py
@@ -35,7 +35,7 @@ t_samp_list = [np.linspace(0, 30, i) for i in n_samples]
 params_seeds = np.arange(20)
 
 env_noise_list = [0., 0.05, 0.1, 0.2]
-meas_noise_list = [0.1, 0.05, 0.1, 0.2]
+meas_noise_list = [0., 0.05, 0.1, 0.2]
 
 n_init_cond = 20
 
@@ -55,10 +55,17 @@ A_diag_std = 0.1 * A_diag_mean
 
 #%%
 
-n_experiments = len(n_sp) * len(n_samples) * len(params_seeds) * len(env_noise_list) * len(meas_noise_list) * n_init_cond * repetitions
-data_size = n_sp.sum() * np.sum(n_samples) * len(params_seeds) * len(env_noise_list) * len(meas_noise_list) * n_init_cond * repetitions * 8 / 1024**2
-print(f"Number of experiments: {n_experiments}")
-print(f"Expected total dataset size: {data_size:.3f} MB")
+n_experiments = len(n_sp) * len(params_seeds) * len(env_noise_list)
+print(f"Number of experiments (= number of files): {n_experiments}")
+
+n_datasets = n_init_cond * repetitions * len(meas_noise_list) * len(n_samples)
+print(f"Number of datasets per file: {n_datasets}")
+
+print(f"Total number of datasets: {n_experiments*n_datasets}")
+
+data_size = (n_sp.sum()+5*len(n_sp)) * np.sum(n_samples) *  len(params_seeds) * len(env_noise_list) *\
+     len(meas_noise_list) * n_init_cond * repetitions * 18 / 1024**2
+print(f"Expected total size: {data_size:.3f} MB")
 
 
 #%%
@@ -79,7 +86,6 @@ for i in range(len(n_sp)):
 
         for k in range(len(env_noise_list)):
             env_noise = env_noise_list[k]
-            save_name = f"{n_sp[i]}_sp{params_seeds[j]}_env_noise{env_noise}"
+            save_name = f"{n_sp[i]}_sp{j}_env_noise{env_noise}"
             gen_replicates(p, env_noise, init_cond_list, t0, dt, t_samp_list, meas_noise_list, repetitions, 
             seed=k, scale_meas_noise_by_abund=True, save_datasets=True, save_loc="test", save_name=save_name)
-
